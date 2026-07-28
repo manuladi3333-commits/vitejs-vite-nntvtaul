d8dbdde873e7820001ce611d0b3e0b63260a9f2e
manuladi3333-commits <manuladi.3333@gmail.com>
2026-07-28 06:41:44 +0530

    c

---
+++ src/Canvas.jsx
@@ -0,0 +1,8 @@
+export default function Canvas() {
+      return (
+          <div>
+                <h2>Canvas Page (new version)</h2>
+                    </div>
+                      );
+                      }
+}
